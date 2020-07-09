54f234a07408e641effd6d071ddf28f85de007d2
Max Culley <48493881+maxculley@users.noreply.github.com>
2020-07-09 11:44:32 +0100

    Added Javadoc comments for NonHR class

--- EmployeeManagementSystemJava/src/main/java/Employee/NonHR.java
+++ EmployeeManagementSystemJava/src/main/java/Employee/NonHR.java
@@ -1,7 +1,26 @@
 package Employee;
 
+/**
+ * This class <b>NonHR</b> extends the class <b>Employee</b>.
+ * 
+ * No additional functionality yet but has potential to have methods which can
+ * bring more functionality to the class with manipulation of data within itself,
+ * unlike the <b>HR</b> class which can interact with all employees.
+ * 
+ */
 public class NonHR extends Employee {
-
+    
+    /**
+     * Creates an NonHR object with data.
+     * 
+     * @param ID Employee's ID
+     * @param firstName Employee's First Name
+     * @param lastName Employee's Last Name
+     * @param address Employee's address
+     * @param age Employee's age
+     * @param gender Employee's gender
+     * @param salary Employee's salary
+     */
     public NonHR(int ID, String firstName, String lastName, String address, int age, String gender, int salary) {
         super (ID, firstName, lastName, address, age, gender, salary);
     }
