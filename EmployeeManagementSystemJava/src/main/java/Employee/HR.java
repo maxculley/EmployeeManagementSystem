a1abe0272c9c0e9b7c4571ba624567f81bed3f8a
Max Culley <48493881+maxculley@users.noreply.github.com>
2020-07-09 11:39:06 +0100

    Added Javadoc comments for HR class

--- EmployeeManagementSystemJava/src/main/java/Employee/HR.java
+++ EmployeeManagementSystemJava/src/main/java/Employee/HR.java
@@ -1,7 +1,25 @@
 package Employee;
 
+/**
+ * This class <b>HR</b> extends the class <b>Employee</b>.
+ * 
+ * No additional functionality yet but has potential to have methods which can
+ * interact with all employees; unlike Non HR which can only interact with itself.
+ * 
+ */
 public class HR extends Employee {
 
+    /**
+     * Creates an HR object with data.
+     * 
+     * @param ID Employee's ID
+     * @param firstName Employee's First Name
+     * @param lastName Employee's Last Name
+     * @param address Employee's address
+     * @param age Employee's age
+     * @param gender Employee's gender
+     * @param salary Employee's salary
+     */
     public HR(int ID, String firstName, String lastName, String address, int age, String gender, int salary) {
         super (ID, firstName, lastName, address, age, gender, salary);
     }
