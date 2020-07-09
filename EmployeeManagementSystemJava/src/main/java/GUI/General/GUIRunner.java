6a7dd755b34fe429e6e5f8072be913a1387f8916
Max Culley <48493881+maxculley@users.noreply.github.com>
2020-07-09 11:58:00 +0100

    Added Javadoc comments for GUIRunner class

--- EmployeeManagementSystemJava/src/main/java/GUI/General/GUIRunner.java
+++ EmployeeManagementSystemJava/src/main/java/GUI/General/GUIRunner.java
@@ -1,7 +1,22 @@
 package GUI.General;
 
-abstract class GUIRunner {
-
+/**
+ * This class <b>GUIRunner</b> is the class used to instantiate the GUIInfo class
+ * to run the project.
+ * 
+ */
+public class GUIRunner {
+    
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private GUIRunner() {}
+    
+    /**
+     * This is the main method.
+     * 
+     * @throws ClassNotFoundException - if the GUIInfo class is not located.
+     */
     public static void main(String[] args) throws ClassNotFoundException {
         new GUIInfo();
     }
